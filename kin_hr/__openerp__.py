588affc06ef940e89c3159e09bbf630d44710ac2
kingsley <kingsleyuk2003@yahoo.com>
2017-05-04 22:16:31 +0100

    change

--- kin_hr/__openerp__.py
+++ kin_hr/__openerp__.py
@@ -12,6 +12,8 @@
 Human Resources Extensions.
 Includes Extra Fields for Employee Directory such as Qualifications, Employment Details,
 Next of Kin, Emergency Information, Guarantor Information etc
+For Help in Customization: Contact Kingsley Okonkwo on +2348030412562
+
 =======================================================================================
 """,
     'author': 'Kinsolve Solutions',
